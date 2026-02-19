5d5e04470f527c2f34d5ca8b0fa4ab9123083647
VictorHugo340 <victorhugos.540@gmail.com>
2026-02-19 11:24:21 -0300

    Criação da Estrutura condicional para validação do Nivel do Herói e saída com a mensagem para o usuário

--- index.js
+++ index.js
@@ -2,6 +2,28 @@
 const prompt = require('prompt-sync')();
 
 //lançamento das variáveis e solicitação ao usuário 
-nomeHeroi = prompt("Digite o nome do Herói: ")
-qtdxp = prompt("Agora, informe a quantidade de XP do herói: ")
-console.log ("O herói de nome " + nomeHeroi + " tem " + qtdxp + " pontos de experiência")
+let nomeHeroi = prompt("Digite o nome do Herói: ")
+let qtdxp = Number(prompt("Agora, informe a quantidade de XP do herói: "))
+let nivelHeroi;
+
+//Estrutura COndicional para validação do Nível do herói
+if (qtdxp <= 1000) {
+    nivelHeroi = "Ferro"
+} else if (qtdxp <= 2000){
+    nivelHeroi = "Bronze"    
+} else if(qtdxp <= 5000){
+    nivelHeroi = "Prata"
+} else if (qtdxp <= 7000) {
+    nivelHeroi = "Ouro"
+} else if (qtdxp <= 8000) {
+    nivelHeroi = "Platina"
+} else if (qtdxp <= 9000) {
+    nivelHeroi = "Ascendente"
+} else if (qtdxp <= 10000) {
+    nivelHeroi = "Imortal"
+} else {
+    nivelHeroi = "Radiante"
+}
+
+//Saída
+console.log ("O herói de nome " + nomeHeroi + " tem " + qtdxp + " XP e está no nível " + nivelHeroi + " !")
